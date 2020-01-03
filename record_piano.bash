df4a7a03ef36f8b5ae6503de3716950dcac8aeef
Dan <JamesDanielBaird@gmail.com>
2020-01-02 23:12:34 -0800

    Initial commit

---
+++ record_piano.bash
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set nounset
+set pipefail
+set noglob
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+piano_data="${script_dir}/pianodata"
+mkdir -p ${piano_data}
+
+sleep_time=30
+
+function listen_to_piano() {
+    aseqdump -p "Digital Piano"
+}
+
+while : # Repeat until interrupt
+do
+    while read -r line; do
+        if [[ "${line}" == *"Note on"* ]]
+        then
+	    echo "Saw note"
+	    todays_date=$(date +"%Y-%m-%d")
+	    current_time=$(date +"%H:%M")
+	    #current_time=$(date +"%T")
+            today_file="${piano_data}/${todays_date}"
+	    touch ${today_file}
+	    if [ $(grep -c "${current_time}" "${today_file}") -eq "0" ];
+            then
+                echo "${current_time}" >> ${today_file}
+	        echo "Writing ${current_time} to ${today_file}"
+            fi
+        fi
+
+        if [[ "${line}" =~ "Port unsubscribed" ]]
+        then
+	    echo "Piano turned off, retrying connection"
+            break
+        fi
+        if [[ "${line}" =~ "Invalid port" ]]
+        then
+	    echo "Piano not connected, retrying"
+            break
+        fi
+
+    done < <(listen_to_piano)
+
+    sleep "${sleep_time}"
+done
